9baee2979a7b72959a94addab4aae92124208443
mayconds000 <mayconds000@gmail.com>
2017-04-25 00:06:12 -0300

    update menu and header

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
-console.log('Hello')
+$(function() {
+  let $headerHome = $('#header-home')
+  const $headerHomeH1 = $('#header-home > h1')[0]
+
+  
+  /**
+   * Functions with scroll
+   */ 
+  $(window).scroll(function() {
+    $wScroll = $(this).scrollTop()
+    
+    if($wScroll > $headerHome.offset().top - 60 && $wScroll < $headerHome.height() + $headerHome.offset().top) {
+      $headerHome.css('transform', 'scale(' + ($wScroll / 5800 + 1) + ')')
+      
+    }
+  });
+
+
+  /**
+   * Functions with fade
+   */
+  const setTime = (elm, func, time) => {
+    console.log(func)
+    setTimeout(() => {
+      fadeIn(elm)
+      leftToRight(elm)
+    }, time);
+  }
+  const fadeIn = (elm) => {
+    elm.style.opacity = 1
+  }
+
+  const leftToRight = (elm) => {
+    elm.style.marginLeft = 0
+  }
+  
+  setTime($headerHomeH1, [fadeIn, leftToRight], 1000)
+})
