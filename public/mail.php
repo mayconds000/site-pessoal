99462579955a8e9f4b2e82fc638e2cd60c14cfbd
Maycon <mayconds000@gmail.com>
2017-05-04 11:38:23 -0300

    added class PHPMailer

--- public/mail.php
+++ public/mail.php
@@ -1,3 +1,43 @@
 <?php
 
-var_dump( $_POST );
+
+require './PHPMailer/PHPMailerAutoload.php';
+//Recupera os dados enviados pelo formulário
+
+
+$getPost = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+
+var_dump( $getPost );
+
+// Variáveis
+$nome = $getPost['nome'];
+$email = $getPost['email'];
+$telefone = $getPost['phone'];
+$mensagem = $getPost['mensagem'];
+
+
+// Enviando o e-mail 
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->Host = "br12.hostgator.com.br";
+$mail->SMTPAuth = true;
+$mail->Username = "site@mayconds.com";
+$mail->Password = "Mxds@326";
+/**
+* Caso o SMTPDebug estiver abilitado da erro no redirect devido ao 
+* `echo` 
+*/
+//$mail->SMTPDebug = 3;
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
+
+$mail->CharSet = "utf8";
+$mail->setFrom("site@mayconds.com", "{$nome}");
+$mail->addAddress("mayconds000@gmail.com");
+$mail->isHTML(true);
+
+$mail->Subject = "Orcamento pelo site";
+$mail->Body = "Enviado por: {$nome} <br /> 
+                  Telefone: {$telefone}<br /> 
+                     Email: {$email}<br /> 
+                  Mensagem: {$mensagem}";
